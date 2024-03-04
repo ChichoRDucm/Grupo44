94f53bb50d63e059088a7219c1407f474a2f207d
José Narciso Robles Durán <josenaro@ucm.es>
2024-03-04 12:07:50 +0100

    initial commit

---
+++ ex00/ft_strdup.c
@@ -0,0 +1,23 @@
+char  *ft_strdup(char *src)
+{
+  char uppercaseA;
+  char lowercaseA;
+  char lowercaseZ;
+  int i;
+
+  uppercaseA = 'A';
+  lowercaseA = 'a';
+  lowercaseZ = 'z';
+
+  i = 0;
+  while (src[i] != '\0')
+  {
+
+    i = i + 1;
+  }
+}
+
+int main(void)
+{
+  return 0;
+}
